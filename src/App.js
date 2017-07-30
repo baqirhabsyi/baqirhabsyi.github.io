749042690a0017d6988a92bdcd54d4acff372d6a
baqirhabsyi <darthbadar@live.com>
2017-07-30 17:07:05 +0700

    Update Add Berita

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { jsonServerRestClient, Admin, Resource } from 'admin-on-rest';
+import { jsonServerRestClient, Admin, Resource, Delete } from 'admin-on-rest';
 import { RestClient } from 'aor-firebase-client';
 import { PostList, JadwalEdit, JadwalCreate } from './posts';
 import { MahasiswaList, MahasiswaEdit } from './mahasiswa';
+import { BeritaList, CreateBerita } from './berita';
 
 const firebaseConfig = {
   apiKey: "AIzaSyD9HeUKR5WJlvy5k6_u9-A9T0cRkRz22ec",
@@ -13,12 +14,13 @@ const firebaseConfig = {
   messagingSenderId: "868404744748"
 };
 
-const trackedResources = ['kelas-testing2', 'kompen', 'hari'];
+const trackedResources = ['kelas-testing2', 'kompen', 'hari', 'posts'];
 
 const App = () => (
     <Admin title="Admin Siskominfo Jadwal dan Kompen" restClient={RestClient(trackedResources, firebaseConfig)}>
-        <Resource options={{ label: 'Jadwal Kelas' }} name="kelas-testing2" list={PostList} edit={JadwalEdit} create={JadwalCreate} />
+        <Resource remove={Delete} options={{ label: 'Jadwal Kelas' }} name="kelas-testing2" list={PostList} edit={JadwalEdit} create={JadwalCreate} />
         <Resource options={{ label: 'Daftar Kompen'}} name="kompen" list={MahasiswaList} edit={MahasiswaEdit} />
+        <Resource options={{ labelL: 'Daftar Berita' }} name="posts" list={BeritaList} create={CreateBerita} />
     </Admin>
 );
 
---
+++ src/berita.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { DateInput, ImageField, Filter, List, Edit, Create, Datagrid, ReferenceField, TextField, EditButton, DisabledInput, LongTextInput, ReferenceInput, SelectInput, SimpleForm, TextInput } from 'admin-on-rest';
+import randomID from 'random-id';
+
+export const BeritaList = (props) => (
+    <List {...props} title="Daftar Berita">
+        <Datagrid bodyOptions={{ showRowHover: true }}>
+            <TextField source="judul" title="Judul" />
+            <TextField source="teaser" title="Teaser" />
+            <TextField source="published_at" title="Tanggal Publikasi" />
+            <EditButton />
+        </Datagrid>
+    </List>
+);
+
+export const CreateBerita = (props) => (
+    <Create {...props}>
+        <SimpleForm>
+            <TextInput style={{ display: 'none' }} source="id" defaultValue={() => randomID()} />
+            <DateInput source="published_at" title="Tanggal Publikasi" options={{
+                mode: 'landscape',
+                minDate: new Date(),
+                hintText: 'Pilih tanggal publikasi',
+                okLabel: 'OK',
+                cancelLabel: 'Cancel'
+            }} />
+            <TextInput source="judul" title="judul" />
+            <TextInput source="teaser" title="Teaser" />
+            <TextInput source="isi" title="Isi" />
+        </SimpleForm>
+    </Create>
+)
